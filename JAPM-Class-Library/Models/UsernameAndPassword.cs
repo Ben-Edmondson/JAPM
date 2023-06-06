c196ec1a19e6cdb32765aad3d841958e3076ac95
ben-edmondson <ben.edmondson99@gmail.com>
2023-06-06 16:27:30 +0100

    nullable bs

--- JAPM-Class-Library/Models/UsernameAndPassword.cs
+++ JAPM-Class-Library/Models/UsernameAndPassword.cs
@@ -8,7 +8,9 @@ namespace JAPM_Class_Library.Models
 {
     public class UsernameAndPassword : AppToSave
     {
+#nullable disable
         public string Username { get; set; }
         public string Password { get; set; }
+#nullable enable
     }
 }
