5a54f892336762ec66e7e676d84d08ac04f87abd
ben-edmondson <ben.edmondson99@gmail.com>
2023-06-06 16:32:48 +0100

    Created interface and repository template.

---
+++ JAPM-Class-Library/Repository/IPasswordRepository.cs
@@ -0,0 +1,19 @@
+﻿using JAPM.ClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAPM.ClassLibrary.Repository
+{
+    public interface IPasswordRepository
+    {
+        public UsernameAndPassword Create(string appName, string userName, string password);
+        public List<UsernameAndPassword> ReadAll();
+        public UsernameAndPassword Read(int id);
+        public bool Update(int id, string userName, string password);
+        public bool Delete(int id);
+        
+    }
+}
---
+++ JAPM-Class-Library/Repository/PasswordRepository.cs
@@ -0,0 +1,39 @@
+﻿using JAPM.ClassLibrary.Models;
+using JAPM.ClassLibrary.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAPM_Class_Library.Repository
+{
+    public class PasswordRepository : IPasswordRepository
+
+    {
+        public UsernameAndPassword Create(string appName, string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UsernameAndPassword Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UsernameAndPassword> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
