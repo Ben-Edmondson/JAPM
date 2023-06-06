643cf4dbaf5ab2c1cca86a3c806ff636b8b9025c
ben-edmondson <ben.edmondson99@gmail.com>
2023-06-06 16:20:26 +0100

    Basic inputs for App name, username, password. Rudimentary validation.

---
+++ JAPM-Class-Library/Inputs.cs
@@ -0,0 +1,46 @@
+﻿namespace JAPM.ClassLibrary
+{
+    public class Inputs
+    {
+
+        public string GetAppNAme()
+        {
+            string? appName = null;
+            Console.WriteLine("Please enter an app you would like to save a password for.");
+            while (appName == null)
+            {
+                appName = Console.ReadLine();
+                if (appName == null)
+                {
+                    Console.WriteLine("Please enter an app you would like to save a password for. This cannot be blank.");
+                }
+            }
+            return appName;
+        }
+        public string GetUsernameOrEmail()
+        {
+            string? username = null;
+            Console.WriteLine("Please enter the Username or Email you would like to save a password for.");
+            while (username == null)
+            {
+                username = Console.ReadLine();
+                if (username == null)
+                {
+                    Console.WriteLine("Please enter the Username or Email you would like to save a password for. This cannot be blank.");
+                }
+            }
+            return username;
+        }
+
+        public string GetPassword()
+        {
+            string? password = null;
+            Console.WriteLine("Please enter the password you would like to save.");
+            if (password == null)
+            {
+                Console.WriteLine("Please enter the password you would like to save. This cannot be blank.");
+            }
+            return password;
+        }
+    }
+}
